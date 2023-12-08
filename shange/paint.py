113f0e17a14b42018c2ed3bc1e7bd4f2f8bf8ba3
majun <2113725201@qq.com>
2023-12-08 16:29:08 +0800

    栅格数据实验画图

--- shange/paint.py
+++ shange/paint.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# 设置中文字体
+plt.rcParams["font.sans-serif"] = ["SimHei"]
+
 data = {
     'PostGresql+PostGis': [3, 7, 16, 35, 58],
     'ArcGis+FileSystem': [2, 9, 23, 49, 70],
@@ -9,27 +12,27 @@ data = {
 }
 x = [0.1, 0.5, 1, 3.7, 5.5]
 
-# Create a figure and two subplots for line chart and table
+# 为折线图和表格创建一个图形和两个子图形
 fig, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [20, 1]}, figsize=(8, 6))
 
-# Plotting the line chart
+# 用点标记绘制折线图
 for label, values in data.items():
-    ax1.plot(x, values, label=label)
+    ax1.plot(x, values, marker='o', label=label)  # 为点标记添加标记=“0”
 
 ax1.legend()
-ax1.set_xlabel('raster data size(GB)')
-ax1.set_ylabel('Build pyramid and put it into storage(min)')
+ax1.set_xlabel('栅格影像大小(GB)')
+ax1.set_ylabel('构建金字塔并入库(min)')
 
-# Creating a DataFrame for the table
+# 为表创建DataFrame
 # table_data = pd.DataFrame(data, index=[str(val) + 'GB' for val in x])
 #
-# # Plotting the table
+# # 绘制表格
 # table = ax2.table(cellText=table_data.values, colLabels=table_data.columns, rowLabels=table_data.index,
 #                   cellLoc='center', rowLoc='center', loc='center')
 
-# Hide the axes of the table
+# 隐藏表格的轴
 ax2.axis('off')
 
-plt.subplots_adjust(hspace=0.15)  # Adjusting the spacing between subplots
+plt.subplots_adjust(hspace=0.15)  # 调整子地块之间的间距
 plt.savefig('fig5.png')
 plt.show()
--- shange/paintsd.py
+++ shange/paintsd.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 
+# 设置中文字体
+plt.rcParams["font.sans-serif"] = ["SimHei"]
+
 plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 
 # 数据
@@ -8,11 +11,12 @@ postgres = [2, 10, 94, 153]
 accumulo = [8, 15, 22, 41]
 
 # 绘图
-plt.plot(sizes, postgres, label="PostgreSQL")
-plt.plot(sizes, accumulo, label="Accumulo")
-plt.xlabel("Extent(km²)")
-plt.ylabel("Read Time(s)")
+plt.plot(sizes, postgres, marker='o', label="PostgreSQL")  # 添加点标记 marker='o'
+plt.plot(sizes, accumulo, marker='o', label="Accumulo")  # 添加点标记 marker='o'
+plt.xlabel("栅格范围(km²)")
+plt.ylabel("读取时间(s)")
 plt.legend()
 
 # 显示图形
+plt.savefig('fig7.png')
 plt.show()
