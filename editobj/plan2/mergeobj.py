63b066433fbc4fe649f715316bdded8b8c6d7e4c
majun <2113725201@qq.com>
2023-12-25 15:53:29 +0800

    整理合并代码(一套将geojson建筑物模与地理obj模型合并成为一个obj文件)

--- editobj/plan2/mergeobj.py
+++ editobj/plan2/mergeobj.py
@@ -1,232 +1,86 @@
-# import numpy as np
-# import open3d as o3d
-# import trimesh
-#
-# def load_obj(file_path):
-#     mesh = o3d.io.read_triangle_mesh(file_path)
-#     return mesh
-#
-# def load_obj2(file_path):
-#     mesh = trimesh.load_mesh(file_path)
-#     return mesh
-#
-# def merge_meshes(mesh1, mesh2):
-#     # 获取顶点、面、纹理坐标和颜色信息
-#     vertices1 = np.asarray(mesh1.vertices)
-#     triangles1 = np.asarray(mesh1.triangles)
-#     triangle_uvs1 = np.asarray(mesh1.triangle_uvs)
-#     vertex_color1 = np.asarray(mesh1.vertex_normals)
-#
-#     vertices2 = np.asarray(mesh2.vertices)
-#     triangles2 = np.asarray(mesh2.triangles)
-#     vertex_color2 = np.asarray(mesh2.vertex_normals)
-#     triangle_uvs2 = np.asarray(mesh2.triangle_uvs)
-#
-#     # 合并顶点、面、纹理坐标和颜色
-#     vertices_combined = np.vstack((vertices1, vertices2 + np.max(vertices1, axis=0)))
-#     triangles_combined = np.vstack((triangles1, triangles2 + len(vertices1)))
-#
-#     # 确保纹理坐标和颜色信息对齐
-#     if len(triangle_uvs1) > 0 and len(triangle_uvs2) > 0:
-#         triangle_uvs_combined = np.vstack((triangle_uvs1, triangle_uvs2))
-#     else:
-#         triangle_uvs_combined = []
-#
-#     if len(vertex_color1) > 0 and len(vertex_color2) > 0:
-#         vertex_color_combined = np.vstack((vertex_color1, vertex_color2))
-#     else:
-#         vertex_color_combined = []
-#
-#     # 创建合并后的Mesh对象
-#     mesh_combined = o3d.geometry.TriangleMesh()
-#     mesh_combined.vertices = o3d.utility.Vector3dVector(vertices_combined)
-#     mesh_combined.triangles = o3d.utility.Vector3iVector(triangles_combined)
-#
-#     if len(triangle_uvs_combined) > 0:
-#         mesh_combined.triangle_uvs = o3d.utility.Vector2dVector(triangle_uvs_combined)
-#
-#     if len(vertex_color_combined) > 0:
-#         mesh_combined.vertex_colors = o3d.utility.Vector3dVector(vertex_color_combined)
-#
-#     return mesh_combined
-#
-# def main():
-#     # 加载带有纹理信息的OBJ文件
-#     mesh_with_texture = load_obj(r"C:\Users\mj\Code\Obj\OBJ\3143415262517261-20-962\model.obj")
-#
-#     # 加载没有纹理信息的OBJ文件
-#     mesh_without_texture = load_obj2(r"C:\Users\mj\Code\Obj\OBJ\new.obj")
-#
-#     # 合并两个Mesh对象
-#     merged_mesh = merge_meshes(mesh_with_texture, mesh_without_texture)
-#
-#     # 保存合并后的OBJ文件
-#     o3d.io.write_triangle_mesh(r"C:\Users\mj\Code\Obj\merged_output.obj", merged_mesh)
-#
-# if __name__ == "__main__":
-#     main()
-
-# import numpy as np
-# import open3d as o3d
-# import trimesh
-#
-# def load_obj(file_path):
-#     mesh = o3d.io.read_triangle_mesh(file_path)
-#     return mesh
-#
-# def load_obj2(file_path):
-#     mesh = trimesh.load(file_path)
-#     return mesh
-#
-# def merge_meshes(mesh1, mesh2):
-#     # 获取顶点和面信息
-#     vertices1 = np.asarray(mesh1.vertices)
-#     triangles1 = np.asarray(mesh1.triangles)
-#     triangle_uvs1 = np.asarray(mesh1.triangle_uvs)
-#     vertex_color1 = np.asarray(mesh1.vertex_normals)
-#
-#     v2 = mesh2.vertices  # 这样得到的v,f格式是trimesh 内置的格式，不能直接用于其它计算，需要转换为numpy
-#     f2 = mesh2.faces
-#     v2 = np.array(v2)
-#     f2 = np.array(f2)
-#     # vertices2 = np.asarray(mesh2.vertices)
-#     # triangles2 = np.asarray(mesh2.triangles)
-#     # triangle_uvs2 = np.asarray(mesh2.triangle_uvs)
-#     # vertex_color2 = np.asarray(mesh2.vertex_colors)
-#
-#     # 合并顶点和面
-#     vertices_combined = np.vstack((vertices1, v2 + np.max(vertices1, axis=0)))
-#     triangles_combined = np.vstack((triangles1, f2 + len(vertices1)))
-#     triangle_uvs_combined = np.vstack((triangle_uvs1, []))
-#     vertex_color_combined = np.vstack((vertex_color1, []))
-#
-#     # 创建合并后的Mesh对象
-#     mesh_combined = o3d.geometry.TriangleMesh()
-#     mesh_combined.vertices = o3d.utility.Vector3dVector(vertices_combined)
-#     mesh_combined.triangles = o3d.utility.Vector3iVector(triangles_combined)
-#     mesh_combined.triangle_uvs = o3d.utility.Vector3dVector(triangle_uvs_combined)
-#     mesh_combined.vertex_colors = o3d.utility.Vector3dVector(vertex_color_combined)
-#
-#     return mesh_combined
-#
-# def main():
-#     # 加载带有纹理信息的OBJ文件
-#     mesh_with_texture = load_obj(r"C:\Users\mj\Code\Obj\OBJ\3143415262517261-20-962\model.obj")
-#
-#     # 加载没有纹理信息的OBJ文件
-#     mesh_without_texture = load_obj2(r"C:\Users\mj\Code\Obj\OBJ\new.obj")
-#
-#     # 合并两个Mesh对象
-#     merged_mesh = merge_meshes(mesh_with_texture, mesh_without_texture)
-#
-#     # 保存合并后的OBJ文件
-#     o3d.io.write_triangle_mesh(r"C:\Users\mj\Code\Obj\merged_output.obj", merged_mesh)
-#
-# if __name__ == "__main__":
-#     main()
-
-# import numpy as np
-# import open3d as o3d
-# import trimesh
-#
-# def load_obj(file_path):
-#     mesh = o3d.io.read_triangle_mesh(file_path)
-#     return mesh
-#
-# def load_obj2(file_path):
-#     mesh = trimesh.load(file_path)
-#     return mesh
-#
-# def merge_meshes(mesh1, mesh2):
-#     # 获取顶点和面信息
-#     vertices1 = np.asarray(mesh1.vertices)
-#     triangles1 = np.asarray(mesh1.triangles)
-#     triangle_uvs1 = np.asarray(mesh1.triangle_uvs)
-#     vertex_normals1 = np.asarray(mesh1.vertex_normals)
-#
-#     v2 = mesh2.vertices
-#     f2 = mesh2.faces
-#     v2 = np.array(v2)
-#     f2 = np.array(f2)
-#
-#     # 合并顶点和面
-#     vertices_combined = np.vstack((vertices1, v2 + np.max(vertices1, axis=0)))
-#     triangles_combined = np.vstack((triangles1, f2 + len(vertices1)))
-#
-#     # 处理纹理坐标和法线信息
-#     if len(triangle_uvs1) > 0:
-#         triangle_uvs_combined = np.vstack((triangle_uvs1, np.zeros_like(triangle_uvs1)))
-#     else:
-#         triangle_uvs_combined = np.zeros((0, 2))
-#
-#     if len(vertex_normals1) > 0:
-#         vertex_normals_combined = np.vstack((vertex_normals1, np.zeros_like(vertex_normals1)))
-#     else:
-#         vertex_normals_combined = np.zeros((0, 3))
-#
-#     # 创建合并后的Mesh对象
-#     mesh_combined = o3d.geometry.TriangleMesh()
-#     mesh_combined.vertices = o3d.utility.Vector3dVector(vertices_combined)
-#     mesh_combined.triangles = o3d.utility.Vector3iVector(triangles_combined)
-#     mesh_combined.triangle_uvs = o3d.utility.Vector2dVector(triangle_uvs_combined)
-#     mesh_combined.vertex_normals = o3d.utility.Vector3dVector(vertex_normals_combined)
-#
-#     return mesh_combined
-#
-# def main():
-#     # 加载带有纹理信息的OBJ文件
-#     mesh_with_texture = load_obj(r"C:\Users\mj\Code\Obj\OBJ\3143415262517261-20-962\model.obj")
-#
-#     # 加载没有纹理信息的OBJ文件
-#     mesh_without_texture = load_obj2(r"C:\Users\mj\Code\Obj\OBJ\new.obj")
-#
-#     # 合并两个Mesh对象
-#     merged_mesh = merge_meshes(mesh_with_texture, mesh_without_texture)
-#
-#     # 保存合并后的OBJ文件
-#     o3d.io.write_triangle_mesh(r"C:\Users\mj\Code\Obj\merged_output.obj", merged_mesh)
-#
-# if __name__ == "__main__":
-#     main()
-
-# import math
-#
-# def make_rotate(rx, ry, rz):
-#     sinX = np.sin(rx)
-#     sinY = np.sin(ry)
-#     sinZ = np.sin(rz)
-#
-#     cosX = np.cos(rx)
-#     cosY = np.cos(ry)
-#     cosZ = np.cos(rz)
-#
-#     Rx = np.zeros((3, 3))
-#     Rx[0, 0] = 1.0
-#     Rx[1, 1] = cosX
-#     Rx[1, 2] = -sinX
-#     Rx[2, 1] = sinX
-#     Rx[2, 2] = cosX
-#
-#     Ry = np.zeros((3, 3))
-#     Ry[0, 0] = cosY
-#     Ry[0, 2] = sinY
-#     Ry[1, 1] = 1.0
-#     Ry[2, 0] = -sinY
-#     Ry[2, 2] = cosY
-#
-#     Rz = np.zeros((3, 3))
-#     Rz[0, 0] = cosZ
-#     Rz[0, 1] = -sinZ
-#     Rz[1, 0] = sinZ
-#     Rz[1, 1] = cosZ
-#     Rz[2, 2] = 1.0
-#
-#     R = np.matmul(np.matmul(Rz, Ry), Rx)
-#     return R
-
 import numpy as np
 import open3d as o3d
 import trimesh
+import math
+import json, copy
+import removeusemtl
+import add_texture_in_mergeobj
+
+def wgs_to_metter(x, y, z):
+    x = x * math.pi / 180
+    y=y*math.pi/180
+    R=6371070
+    a=(math.cos(y)*R)*math.cos(x)
+    b=(math.cos(y)*R)*math.sin(x)
+    c=math.sin(y)*R
+    # x=x*math.pi/180
+    # y=y*math.pi/180
+    # a = math.sqrt((1-(math.sin(y))**2)*((z+6371000)**2)/(1+math.tan(x)))
+    # b = a*math.tan(x)
+    # c = math.sin(y)*(z+6378137)
+    return a,b,c
+
+def wgs_to_metter_height(x, y, z):
+    x = x * math.pi / 180
+    y=y*math.pi/180
+    R=6371070+z
+    a=(math.cos(y)*R)*math.cos(x)
+    b=(math.cos(y)*R)*math.sin(x)
+    c=math.sin(y)*R
+
+    # x=x*math.pi/180
+    # y=y*math.pi/180
+    # a = math.sqrt((1-(math.sin(y))**2)*((z+6371000)**2)/(1+math.tan(x)))
+    # b = a*math.tan(x)
+    # c = math.sin(y)*(z+6378137)
+    return a,b,c
+
+def geojson_to_obj(geojson_file, obj_file):
+    with open(geojson_file, 'r') as f:
+        geojson_data = json.load(f)
+
+    vertices = []
+    faces = []
+
+    for feature in geojson_data['features']:
+        if feature['geometry']['type'] == 'Point':
+            coordinates = feature['geometry']['coordinates']
+            vertices.append(coordinates)
+        elif feature['geometry']['type'] == 'LineString':
+            coordinates = feature['geometry']['coordinates']
+            vertices.extend(coordinates)
+            faces.append(list(range(len(vertices) - len(coordinates), len(vertices))))
+        elif feature['geometry']['type'] == 'Polygon':
+            coordinates = feature['geometry']['coordinates'][0]  # Assuming only exterior ring
+            coordinates_up=copy.deepcopy(coordinates)
+            for i in coordinates:
+                i[0], i[1], z=wgs_to_metter(i[0], i[1],30.0)
+                # i[0]-=174.064252
+                # i[1]-=463.469967
+                # z+=360.203842594102
+                i.append(z)
+                # x,y,z=Xyz2Blh(i[0], i[1], 2696351.106957404)
+                # print(x,y,z)
+            vertices.extend(coordinates)
+            faces.append(list(range(len(vertices) - len(coordinates), len(vertices))))
+            for i in coordinates_up:
+                i[0], i[1], z = wgs_to_metter_height(i[0], i[1], 30.0)
+                # i[0]-=174.064252
+                # i[1]-=463.469967#绿色
+                # z+=360.203842594102#蓝36.69913
+                i.append(z)
+            vertices.extend(coordinates_up)
+            faces.append(list(range(len(vertices) - len(coordinates), len(vertices))))
+            for i in range(len(coordinates) - 1):
+                faces.append([len(vertices) - 2 * len(coordinates) + i, len(vertices) - 2 * len(coordinates) + i + 1,
+                              len(vertices) - len(coordinates) + i + 1, len(vertices) - len(coordinates) + i])
+
+        with open(obj_file, 'w') as f:
+            for vertex in vertices:
+                f.write(f"v {vertex[0]} {vertex[1]} {vertex[2]}\n")
+
+            for face in faces:
+                f.write("f " + " ".join(str(v + 1) for v in face) + "\n")
 
 def load_obj(file_path):
     mesh = trimesh.load(file_path)
@@ -236,35 +90,37 @@ def load_obj_with_texture(file_path):
     mesh = o3d.io.read_triangle_mesh(file_path)
     return mesh
 
-def map_vertices_to_texture_coordinates(vertices, triangle_uvs1):
-    mapped_texture_coordinates = vertices + triangle_uvs1
-    return np.array(mapped_texture_coordinates)
-
-def merge_meshes(mesh1, mesh2):
+def merge_meshes(mesh1_v, mesh2):
     # 获取顶点和面信息
-    vertices1 = np.asarray(mesh1.vertices)
-    triangles1 = np.asarray(mesh1.triangles)
-    triangle_uvs1 = np.asarray(mesh1.triangle_uvs)
-    vertex_normals1 = np.asarray(mesh1.vertex_normals)
+    vertices1 = mesh1_v.vertices
+    triangles1 = mesh1_v.faces
+    vertex_normals1 = mesh1_v.vertex_normals
+    # arr = []
+    # for vertices in range(len(vertices1)):
+    #     tme = []
+    #     for ve in range(len(vertices1[vertices])):
+    #         arr.append(vertices1[vertices][ve])
+    # arr = np.array(arr)
+    # vertices2 = np.asarray(mesh1_vt.vertices)
+    # triangles1 = np.asarray(mesh1_vt.triangles)
+    # triangles2 = mesh1_v.triangles
+    # triangle_uvs1 = np.asarray(mesh1_vt.triangle_uvs)
+    # vertex_normals1 = np.asarray(mesh1_vt.vertex_normals)
 
     v2 = mesh2.vertices
     f2 = mesh2.faces
     v2 = np.array(v2)
     f2 = np.array(f2)
 
-    # 映射new.obj的顶点到model.obj的纹理坐标
-    # mapped_texture_coordinates = map_vertices_to_texture_coordinates(v2, triangle_uvs1)
-
     # 合并顶点和面
     f2 = np.array(f2) + np.shape(vertices1)[0]
     v = np.concatenate((vertices1, v2), axis=0)
     f = np.concatenate((triangles1, f2), axis=0)
 
-    # 合并纹理坐标
-    # if len(triangle_uvs1) > 0:
-    #     triangle_uvs_combined = np.hstack((triangle_uvs1, mapped_texture_coordinates))
-    # else:
-    #     triangle_uvs_combined = np.zeros_like(mapped_texture_coordinates)
+    if len(vertex_normals1) > 0:
+        vertex_normals_combined = np.concatenate((vertex_normals1, np.zeros_like(vertex_normals1)))
+    else:
+        vertex_normals_combined = np.zeros((0, 3))
 
     if len(vertex_normals1) > 0:
         vertex_normals_combined = np.concatenate((vertex_normals1, np.zeros_like(vertex_normals1)))
@@ -282,18 +138,30 @@ def merge_meshes(mesh1, mesh2):
     return obj
 
 def main():
-    # 加载带有纹理信息的OBJ文件
-    mesh_with_texture = load_obj_with_texture(r"C:\Users\mj\Code\Obj\OBJ\3143415262517261-20-962\model.obj")
+    # 建筑物json转obj
+    geojson_file = r"C:\Users\mj\Code\Obj\new.json"  # json文件地址
+    bulid_obj_file = r"C:\Users\mj\Code\Obj\OBJ\new.obj"  # 建筑物obj临时文件
+    geojson_to_obj(geojson_file, bulid_obj_file)
 
-    # 加载没有纹理信息的OBJ文件
-    mesh_without_texture = load_obj(r"C:\Users\mj\Code\Obj\OBJ\new.obj")
+    # 由于地理obj文件由多个切片构成，首先对其进行合并，使用cloudcompare软件对文件进行合并
+    input_file_path = r"C:\Users\mj\Code\Obj\removeuse\Merged mesh.obj"  # 合并后的地理obj文件地址
+    output_file_path = r"C:\Users\mj\Code\Obj\removeuse\mesh.obj"  # 删除合并地理obj文件中的usemtl行（处理后的文件地址）
 
-    # 合并两个Mesh对象
-    merged_mesh = merge_meshes(mesh_with_texture, mesh_without_texture)
+    # 删除合并地理obj文件中的usemtl行
+    removeusemtl.remove_usemtl_lines(input_file_path, output_file_path)
 
-    # 保存合并后的OBJ文件
-    # o3d.io.write_triangle_mesh(r"C:\Users\mj\Code\Obj\merged_output.obj", merged_mesh)
-    merged_mesh.export(r"C:\Users\mj\Code\Obj\merge1.obj")
+    # 加载处理后的地理obj文件
+    mesh_with_v = load_obj(output_file_path)
+
+    # 加载没有纹理信息的OBJ文件（建筑物obj文件地址）
+    mesh_without_texture = load_obj(bulid_obj_file)
+
+    # 合并两个Mesh对象（合并两个obj文件，处理后的obj文件中只有v、vn、f信息还需要进行纹理贴图）
+    merged_mesh = merge_meshes(mesh_with_v, mesh_without_texture)
+
+    # 对合并后的obj进行纹理贴图，outfile_obj为纹理贴图后的保存地址
+    outfile_obj = r"C:\Users\mj\Code\Obj\merge\new_obj3.obj"
+    add_texture_in_mergeobj.merge_meshes(merged_mesh, outfile_obj)
 
 if __name__ == "__main__":
     main()
