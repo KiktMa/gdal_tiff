9f967334014d9cd90f16f9acf11a0a10f7290b49
majun <2113725201@qq.com>
2023-12-26 17:40:52 +0800

    将一个obj中的所有几何体合并为一个集合体结构

---
+++ editobj/plan2/oenfile_merge.py
@@ -0,0 +1,50 @@
+def merge_obj(input_paths, output_path):
+    # 存储所有集合体的顶点、法线、纹理坐标和面
+    all_vertices = []
+    all_normals = []
+    all_tex_coords = []
+    all_faces = []
+
+    for input_path in input_paths:
+        with open(input_path, 'r') as file:
+            vertices = []
+            normals = []
+            tex_coords = []
+            faces = []
+
+            for line in file:
+                parts = line.strip().split()
+
+                if not parts:
+                    continue
+
+                if parts[0] == 'v':
+                    vertices.append(list(map(float, parts[1:])))
+                elif parts[0] == 'vn':
+                    normals.append(list(map(float, parts[1:])))
+                elif parts[0] == 'vt':
+                    tex_coords.append(list(map(float, parts[1:])))
+                elif parts[0] == 'f':
+                    faces.append([list(map(int, v.split('/'))) for v in parts[1:]])
+
+            all_vertices.extend(vertices)
+            all_normals.extend(normals)
+            all_tex_coords.extend(tex_coords)
+            all_faces.extend(faces)
+
+    # 写入新的OBJ文件
+    with open(output_path, 'w') as outfile:
+        for vertex in all_vertices:
+            outfile.write(f"v {' '.join(map(str, vertex))}\n")
+        for normal in all_normals:
+            outfile.write(f"vn {' '.join(map(str, normal))}\n")
+        for tex_coord in all_tex_coords:
+            outfile.write(f"vt {' '.join(map(str, tex_coord))}\n")
+        for face in all_faces:
+            outfile.write(f"f {' '.join(['/'.join(map(str, v)) for v in face])}\n")
+
+# if __name__ == "__main__":
+#     input_paths = [r"C:\Users\mj\Code\Obj\OBJ\3143415262517261-20-962\model.obj"]  # 替换为实际的OBJ文件路径
+#     output_path = r"C:\Users\mj\Code\Obj\OBJ\3143415262517261-20-962\merged_obj.obj"  # 输出OBJ文件路径
+#
+#     merge_obj(input_paths, output_path)
