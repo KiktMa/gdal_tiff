942a290a9cf60e23be5ed1b02d9a757e1b8dadad
majun <2113725201@qq.com>
2024-01-10 16:55:01 +0800

    自动化修改obj中顶点信息过程

--- editobj/ecef2enu/CorTransfer.py
+++ editobj/ecef2enu/CorTransfer.py
@@ -2,7 +2,7 @@ import geo
 from numpy import *
 import numpy as np
 
-with open(r"C:\Users\mj\Code\Obj\merge\new_obj333.obj", 'r') as file:
+with open(r"C:\Users\mj\Desktop\obj\test\Merged mesh.obj", 'r') as file:
     vertices = []
     new_vertices = []
     for line in file:
@@ -14,9 +14,17 @@ with open(r"C:\Users\mj\Code\Obj\merge\new_obj333.obj", 'r') as file:
         if parts[0] == 'v':
             vertices.append(list(map(float, parts[1:])))
 
+    # 25.040609531680197, 121.2533545134789
+    # 24.980294521860554 121.24806089167481
+    # 24.980427736593345 121.2537309245741
+    R = 6371068
+    index = int(len(vertices) / 2)
+    lat = math.asin(vertices[index][2] / R) * 180 / math.pi
+    lon = math.acos(vertices[index][0] / (math.cos(math.asin(vertices[index][2] / R)) * R)) * 180 / math.pi
+    print(lat, lon)
     for vertice in range(len(vertices)):
         arr = geo.ecef_to_enu(vertices[vertice][0], vertices[vertice][1], vertices[vertice][2],
-                              25.040609531680197, 121.2533545134789, 0.0)
+                              lat, lon, 0.0)
         arr = list(arr)
         arr[0], arr[2] = arr[2], arr[0]
         arr[0], arr[1] = arr[1], arr[0]
@@ -31,9 +39,19 @@ with open(r"C:\Users\mj\Code\Obj\merge\new_obj333.obj", 'r') as file:
         rotated_point = np.dot(rotation_matrix, arr)
 
         # arr[0], arr[2] = arr[2], arr[0]
-        arr = tuple(arr)
+        arr = tuple(rotated_point)
         new_vertices.append(arr)
 
-with open(r"C:\Users\mj\Desktop\output.txt", 'w') as outfile:
-    for vertex in new_vertices:
-        outfile.write(f"v {' '.join(map(str, vertex))}\n")
+with open(r"C:\Users\mj\Desktop\obj\test\Merged mesh.obj", 'r') as infile:
+    lines = infile.readlines()
+
+with open(r"C:\Users\mj\Desktop\obj\test\Merged mesh.obj", 'w') as outfile:
+    # for vertex in new_vertices:
+    #     outfile.write(f"v {' '.join(map(str, vertex))}\n")
+    v_len = len(new_vertices)
+    for i, line in enumerate(lines):
+        if line.startswith("v "):
+            vertex = new_vertices[(i-2) // 1]  # 每行三个顶点，减2是因为new_vertices索引开始时为2，因为obj中前两行为cloudcompare合并时锁产生的注释
+            outfile.write(f"v {' '.join(map(str, vertex))}\n")
+        else:
+            outfile.write(line)
